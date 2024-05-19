1ffb8f76df12ec4c1340c90be132e5e3902ae912
VinnikA <vamposht@gmail.com>
2024-05-19 11:40:25 +0300

    small product card is ready

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -2,6 +2,7 @@ import { categories, search } from "./search.js";
 import { timer } from "./timer.js";
 import { formHandelr } from "./form.js";
 import { bigCard } from "./bigCard.js";
+import { smallCard } from "./smallCard.js";
 
 document.addEventListener("DOMContentLoaded", function() {
   categories()
@@ -10,4 +11,5 @@ document.addEventListener("DOMContentLoaded", function() {
   formHandelr()
   bigCard('top-sellers')
   bigCard('week-sale')
+  smallCard('new-goods')
 })
