7f397340ea77e1721df6d3c2981625bc0f0a4402
VinnikA <vamposht@gmail.com>
2024-05-20 21:18:57 +0300

    not found page added

---
+++ frontend/js/data.js
@@ -0,0 +1,16 @@
+export const data = [
+  { 
+    id: 'string',
+    title: 'string',
+    description: 'string',
+    productType: 'string',
+    category: 'string',
+    subcategory: 'string',
+    regularPrice: 0,
+    specialPrice: 0,
+    freeShipping: false,
+    discountOnSecondItem: false,
+    discountOnFirstOrder: false,
+    availableQuantity: 0
+  }
+]
