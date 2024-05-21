5f2f58c90d2d32629a8b5f167a2061d3f0ea3ec6
VinnikA <vamposht@gmail.com>
2024-05-21 15:33:20 +0300

    work with nutrition store page

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -3,13 +3,28 @@ import { timer } from "./timer.js";
 import { formHandelr } from "./form.js";
 import { bigCard } from "./bigCard.js";
 import { smallCard } from "./smallCard.js";
+import { storeRows } from "./storeRows.js";
+
+import { data } from "./data.js";
 
 document.addEventListener("DOMContentLoaded", function() {
-  categories()
-  search()
-  timer()
-  formHandelr()
-  bigCard('top-sellers')
-  bigCard('week-sale')
-  smallCard('new-goods')
+
+  const page = window.location.pathname
+
+  if(page.includes('index')) {
+    categories()
+    search()
+    timer()
+    formHandelr()
+    bigCard('top-sellers')
+    bigCard('week-sale')
+    smallCard('new-goods')
+  }
+
+  if(page.includes('nutrition-store')) {
+    categories()
+    search()
+    storeRows()
+  }
+
 })
---
+++ frontend/js/storeRows.js
@@ -0,0 +1,39 @@
+import { data } from "./data.js"
+
+export function storeRows() {
+
+  const template = document.querySelector('.small-card-template')
+
+  const containers = document.querySelectorAll('.products__cards')
+
+  const renderItem = (data, container) => {
+
+    const clone = template.content.cloneNode(true)
+
+    clone.querySelector('.small-card__img').src = data.productImg 
+    clone.querySelector('.small-card__title').textContent = data.title 
+    clone.querySelector('.small-card__new-price').textContent = `$${data.specialPrice}` 
+    clone.querySelector('.small-card__old-price').textContent = `$${data.regularPrice}` 
+
+    if(!data.freeShipping) {
+      clone.querySelector('#free-shipping').style.display = 'none'
+    }
+    if(!data.discountOnSecondItem) {
+      clone.querySelector('#discount-on-the-second-item').style.display = 'none'
+    }
+    if(!data.discountOnFirstOrder) {
+      clone.querySelector('#discount-on-first-order').style.display = 'none'
+    }
+
+    container.appendChild(clone)
+  }
+
+  containers.forEach(container => {
+    
+    const items = data.filter(item => item.category === container.id)
+
+    items.forEach(item => renderItem(item, container))
+
+  })
+
+}
